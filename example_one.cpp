8e2238a7795f14090eef8eb1124b7540fbafa7a7
Fernando Sanchez <fernandosanchez955@gmail.com>
2022-05-30 21:22:09 -0400

    Beginner file for C++ programming

---
+++ example_one.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std;
+
+int main(){
+    /*Exercises for Chapter 1 page 13*/
+    cout << "Oh what" << endl;
+    cout << "a happy day!" << endl;
+    cout << "Oh yes" << endl;
+    cout << "what a happy day" << endl;
+    return 0;
+}
