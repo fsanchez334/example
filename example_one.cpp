6a0e2e1825dbac08446ead832645d908db9b9dfb
Fernando Sanchez <fernandosanchez955@gmail.com>
2022-05-30 21:24:30 -0400

    Deleted a few lines

--- example_one.cpp
+++ example_one.cpp
@@ -5,7 +5,5 @@ int main(){
     /*Exercises for Chapter 1 page 13*/
     cout << "Oh what" << endl;
     cout << "a happy day!" << endl;
-    cout << "Oh yes" << endl;
-    cout << "what a happy day" << endl;
     return 0;
 }
